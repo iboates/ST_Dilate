f4ef6931addc476d9933f8d64e0e23aba8beacd1
Isaac Boates <iboates@gmail.com>
2018-04-16 20:40:54 +0200

    bug, desired area not being calculated ocrrectly

--- percent_buffer.sql
+++ percent_buffer.sql
@@ -9,46 +9,41 @@
 RETURNS GEOMETRY AS
 
 $$
-
 DECLARE
-
 	current_area FLOAT;
-	desired_area FLOAT = ST_Area(ST_Buffer(in_geom, step))*scale_factor;
+	desired_area FLOAT = ST_Area(in_geom)*scale_factor;
 	dev FLOAT;
 	old_dev FLOAT;
 	safety_counter INTEGER = 0;
 	
 BEGIN
 
-current_area = ST_Area(ST_Buffer(in_geom, buff));
+current_area = ST_Area(in_geom);
 dev = (current_area-desired_area)/desired_area;
 
-RAISE NOTICE 'current_area: %', current_area;
-RAISE NOTICE 'desired_area %', desired_area;
-RAISE NOTICE 'dev (created polygon is % percent bigger/smaller than the desired polygon', dev*100;
-
-WHILE ABS(dev) > tol AND safety_counter < 1000 LOOP
+WHILE ABS(dev) > tol AND safety_counter < 500 LOOP
 
 	IF dev < 0 THEN /* current area is smaller than desired area, increase the step */
-		buff = buff + step;
-	ELSE /* current area is larger than desired area, decrease the step */
 		buff = buff - step;
+	ELSE /* current area is larger than desired area, decrease the step */
+		buff = buff + step;
 	END IF;
 
 	IF dev * old_dev < 0 THEN /* negative value indicates difference of sign, need to do a halving & directional reversal */
 		step = step*-0.5;
 	END IF;
 
-	current_area = ST_Area(ST_Buffer(in_geom, buff));
+	current_area = ST_Area(in_geom);
 	old_dev = dev;
 	dev = (current_area-desired_area)/desired_area;
 
 	safety_counter = safety_counter + 1;
 -- 	RAISE NOTICE 'safety_counter: %', safety_counter;
--- 
--- 	RAISE NOTICE 'current_area: %', current_area;
--- 	RAISE NOTICE 'desired_area %', desired_area;
--- 	RAISE NOTICE '    dev: %', dev;
+
+	RAISE NOTICE 'current_area: %', current_area;
+	RAISE NOTICE 'desired_area %', desired_area;
+ 	RAISE NOTICE '    dev: %', dev;
+	RAISE NOTICE '%', buff;
 -- 	RAISE NOTICE 'old_dev: %', old_dev;
 -- 	RAISE NOTICE 'step: %', step;
 
@@ -65,7 +60,7 @@ DROP TABLE IF EXISTS buffresult;
 CREATE TABLE buffresult AS (
 SELECT
 	id AS id,
-	ST_Area(ST_PercentBuffer(geom, 1.1, 500)) AS geom
+	ST_PercentBuffer(geom, 1.35, 100) AS geom
 FROM
 	public.shrinkpoly
 );
