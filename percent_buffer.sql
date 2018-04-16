af1bf235a76264a7c7934c73f72490a835eaae0c
Isaac Boates <iboates@gmail.com>
2018-04-16 16:07:01 +0200

    able to declare variable

--- percent_buffer.sql
+++ percent_buffer.sql
@@ -1,16 +1,26 @@
-﻿CREATE OR REPLACE FUNCTION ST_PercentBuffer(
+﻿DROP FUNCTION st_percentbuffer(geometry,double precision,double precision);
+
+CREATE OR REPLACE FUNCTION ST_PercentBuffer(
 	in_geom GEOMETRY,
-	buff_pct FLOAT,
-	step FLOAT
+	in_buff_pct FLOAT,
+	in_step FLOAT DEFAULT 100
 )
 
 RETURNS GEOMETRY AS
 
 $$
+DECLARE step FLOAT;
 BEGIN
 
-
+RETURN ST_Buffer(in_geom, 10);
 
 END
 $$
 LANGUAGE plpgsql;
+
+SELECT
+	id AS id,
+	ST_Area(geom) AS geom,
+	ST_Area(ST_PercentBuffer(geom, 1.0, 1.0)) AS pct_buff_geom
+FROM
+	public.shrinkpoly;
